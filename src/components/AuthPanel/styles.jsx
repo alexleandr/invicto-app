67c3fb5a811b1957f1e93f2514514aa9a842bf00
Alex Leandro <alexorcizamus@gmail.com>
2023-02-23 18:57:48 -0300

    :recycle: refactor: Transformando as classNames do AuthPanel em styled-components

--- src/components/AuthPanel/styles.jsx
+++ src/components/AuthPanel/styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import * as Tabs from "@radix-ui/react-tabs"
 
 export const AuthPanelContainer = styled.div`
     width: 60%;
@@ -11,52 +12,52 @@ export const AuthPanelContainer = styled.div`
     img {
         height: 40px;
     }
+`
 
-    .tabs-root {
-        width: 100%;
+export const TabsRoot = styled(Tabs.Root)`
+    width: 100%;
 
-        display: flex;
-        flex-direction: column;
+    display: flex;
+    flex-direction: column;
+`
 
-        .tabs-list {
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
+export const TabsList = styled(Tabs.List)`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
 
-            .tabs-trigger {
-                background-color: transparent;
-                padding: 1rem 0;
-                border: none;
-                border-bottom: 2px solid #363739;
-                color: #ffffff;
+export const TabsTrigger = styled(Tabs.Trigger)`
+    background-color: transparent;
+    padding: 1rem 0;
+    border: none;
+    border-bottom: 2px solid #363739;
+    color: #ffffff;
     
-                font-family: 'Inter', sans-serif;
-                font-size: 1rem;
-                text-align: center;
-                text-decoration: none;
+    font-family: 'Inter', sans-serif;
+    font-size: 1rem;
+    text-align: center;
+    text-decoration: none;
     
-                &:hover {
-                    border-bottom: 2px solid #ff4147;
-                    color: #ff4147;
+    &:hover {
+        border-bottom: 2px solid #ff4147;
+        color: #ff4147;
                     
-                    cursor: pointer;
-                    transition: 0.7s;
-                }
-
-                &[data-state='active'] {
-                    border-bottom: 2px solid #ff4147;
-                    color: #ff4147;
-                }
-
-                &:focus {
-                    border-bottom: 2px solid #ff4147;
-                    color: #ff4147;
-                    outline: none;
-                }
-            }
-        }
-
-        .tabs-content {
-            outline: none;
-        }
+        cursor: pointer;
+        transition: 0.7s;
     }
+
+    &[data-state='active'] {
+        border-bottom: 2px solid #ff4147;
+        color: #ff4147;
+    }
+
+    &:focus {
+        border-bottom: 2px solid #ff4147;
+        color: #ff4147;
+        outline: none;
+    }
+`
+
+export const TabsContent = styled(Tabs.Content)`
+    outline: none;
 `
