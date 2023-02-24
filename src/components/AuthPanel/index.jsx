3b68eea91d53b552b39cae8047264eb873b47437
Alex Leandro <alexorcizamus@gmail.com>
2023-02-24 19:20:06 -0300

    :recycle: refactor: Refatoração do componente AuthPanel

--- src/components/AuthPanel/index.jsx
+++ src/components/AuthPanel/index.jsx
@@ -1,31 +1,29 @@
 import { LoginForm } from "./LoginForm"
 import { RegisterForm } from "./RegisterForm"
-import { AuthPanelContainer, TabsContent, TabsList, TabsRoot, TabsTrigger} from "./styles"
+import { AuthPanelContainer, TabsContent, TabsList, TabsTrigger} from "./styles"
 
 import icon from "../../assets/img/invicto-icon.png"
 
 export function AuthPanel() {
     return (
-        <AuthPanelContainer>
+        <AuthPanelContainer defaultValue="login">
             <img src={icon} alt="Ícone do inVicto." />
 
-            <TabsRoot defaultValue="tab1">
-                <TabsList aria-label="Gerenciamento de contas">
-                    <TabsTrigger value="tab1">
-                        Acessar conta
-                    </TabsTrigger>
-                    <TabsTrigger value="tab2">
-                        Criar conta
-                    </TabsTrigger>
-                </TabsList>
+            <TabsList>
+                <TabsTrigger value="login">
+                    Acessar conta
+                </TabsTrigger>
+                <TabsTrigger value="register">
+                    Criar conta
+                </TabsTrigger>
+            </TabsList>
 
-                <TabsContent value="tab1">
-                    <LoginForm />
-                </TabsContent>
-                <TabsContent value="tab2">
-                    <RegisterForm />
-                </TabsContent>
-            </TabsRoot>
+            <TabsContent value="login">
+                <LoginForm />
+            </TabsContent>
+            <TabsContent value="register">
+                <RegisterForm />
+            </TabsContent>
         </AuthPanelContainer>
     )
 }
